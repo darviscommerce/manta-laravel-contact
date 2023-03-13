ed5029ac1d2eec060bf5e1983e8653b50a241dc6
Arvid de Jong <arvid@darvis.nl>
2023-03-13 23:33:24 +0100

    Route update

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
-use Manta\LaravelContact\Http\Livewire\Contact\ContactCreate;
-use Manta\LaravelContact\Http\Livewire\Contact\ContactList;
-use Manta\LaravelContact\Http\Livewire\Contact\ContactUpdate;
+use App\Http\Livewire\Contact\ContactCreate;
+use App\Http\Livewire\Contact\ContactList;
+use App\Http\Livewire\Contact\ContactUpdate;
 use Illuminate\Support\Facades\Route;
-use Manta\LaravelContact\Http\Livewire\Contact\ContactUploads;
 
 Route::get('/contact', ContactList::class)->name('manta.contact.list');
 Route::get('/contact/toevoegen', ContactCreate::class)->name('manta.contact.create');
